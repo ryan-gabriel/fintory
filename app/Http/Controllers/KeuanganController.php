53a961c2415269883c716df546fd739ff54b0bba
ryan-gabriel <ryangs112233@gmail.com>
2025-05-27 10:56:38 +0700

    add controller for keuangan menu

---
+++ app/Http/Controllers/KeuanganController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CashLedger;
+use Illuminate\Http\Request;
+
+class KeuanganController extends Controller
+{
+    public function index(){
+        $cashLedger = CashLedger::with(['outlet'])
+            ->orderBy('tanggal', 'desc')
+            ->paginate(10);
+        return view('cash-ledger', compact('cashLedger'));
+    }
+
+    public function create(){
+        return view('');
+    }
+    
+    public function store(){
+        return view('cash-ledger');
+    }
+    
+    // public function edit(Request $request): Response
+    // {
+    //     return Inertia::render('Profile/Edit', [
+    //         'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
+    //         'status' => session('status'),
+    //     ]);
+    // }
+
+    // /**
+    //  * Update the user's profile information.
+    //  */
+    // public function update(ProfileUpdateRequest $request): RedirectResponse
+    // {
+    //     $request->user()->fill($request->validated());
+
+    //     if ($request->user()->isDirty('email')) {
+    //         $request->user()->email_verified_at = null;
+    //     }
+
+    //     $request->user()->save();
+
+    //     return Redirect::route('profile.edit');
+    // }
+
+    // /**
+    //  * Delete the user's account.
+    //  */
+    // public function destroy(Request $request): RedirectResponse
+    // {
+    //     $request->validate([
+    //         'password' => ['required', 'current_password'],
+    //     ]);
+
+    //     $user = $request->user();
+
+    //     Auth::logout();
+
+    //     $user->delete();
+
+    //     $request->session()->invalidate();
+    //     $request->session()->regenerateToken();
+
+    //     return Redirect::to('/');
+    // }
+}
