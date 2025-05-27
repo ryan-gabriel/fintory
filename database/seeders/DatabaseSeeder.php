4a63f45f2ba931e3300806da058d85fec9aca3bc
Arsya <azelord18@gmail.com>
2025-05-27 15:51:10 +0700

    update db to include roles and user roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('12345678'),
         ]);
