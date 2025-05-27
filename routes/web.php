95bf261ff533a030194c7101d6201c50adcdd554
ryan-gabriel <ryangs112233@gmail.com>
2025-05-27 11:11:11 +0700

    add route for keuangan menu. not completed yet

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KeuanganController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,16 +9,19 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard', [
-        'current_user' => 'Alexander Pierce',
-        'profile_url' => 'https://placehold.co/400'
-    ]);
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard/keuangan')->name('keuangan.')->group(function () {
+        Route::get('/kas-ledger', [KeuanganController::class, 'index'])->name('index');
+        Route::get('/kas-ledger/create', [KeuanganController::class, 'create'])->name('create');
+        Route::post('/kas-ledger', [KeuanganController::class, 'store'])->name('store');
+    });
 });
 
 require __DIR__.'/auth.php';
