8965e8167ea8292774d3381e9e0ae27f6284112a
ryan-gabriel <ryangs112233@gmail.com>
2025-05-27 11:00:29 +0700

    add kas-ledger view

---
+++ resources/views/kas-ledger.blade.php
@@ -0,0 +1,106 @@
+<x-admin-layout>
+    <div class="my-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <div class="overflow-x-auto relative p-5 sm:rounded-lg">
+                        <table class="w-full text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                            id="pagination-table">
+                            <thead class="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th data-type="date">
+                                        <span class="flex items-center hover:brightness-75 transition-all duration-200 text-base">
+                                            Tanggal
+                                            <svg class="w-4 h-4 ms-1" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                            </svg>
+                                        </span>
+                                    </th>
+                                    <th>
+                                        <span class="flex items-center hover:brightness-75 transition-all duration-200 text-base">
+                                            Tipe Transaksi
+                                            <svg class="w-4 h-4 ms-1" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                            </svg>
+                                        </span>
+                                    </th>
+                                    <th>
+                                        <span class="flex items-center hover:brightness-75 transition-all duration-200 text-base">
+                                            Sumber
+                                            <svg class="w-4 h-4 ms-1" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                            </svg>
+                                        </span>
+                                    </th>
+                                    <th>
+                                        <span class="flex items-center hover:brightness-75 transition-all duration-200 text-base">
+                                            Jumlah
+                                            <svg class="w-4 h-4 ms-1" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                            </svg>
+                                        </span>
+                                    </th>
+                                    <th>
+                                        <span class="flex items-center hover:brightness-75 transition-all duration-200 text-base">
+                                            Saldo Akhir
+                                            <svg class="w-4 h-4 ms-1" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                            </svg>
+                                        </span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($cashLedger as $item)
+                                    <tr
+                                        class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                                        <td>{{ $item->created_at->format('Y-m-d') }}</td>
+                                        <td>{{ $item->tipe }}</td>
+                                        <td>{{ $item->sumber }}</td>
+                                        <td>{{ $item->amount }}</td>
+                                        <td>{{ $item->saldo_setelah }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@9.0.3"></script>
+    <script>
+        if (document.getElementById("pagination-table") && typeof simpleDatatables.DataTable !== 'undefined') {
+            const dataTable = new simpleDatatables.DataTable("#pagination-table", {
+                paging: true,
+                perPage: 5,
+                perPageSelect: [5, 10, 15, 20, 25],
+                sortable: true,
+            });
+        }
+    </script>
+
+</x-admin-layout>
