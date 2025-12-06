4e4be1258c32570d6c7b38887050dcc9a8fd825a
Muhammad Ali Nur Rohman <m.alinurrohman10@upi.edu>
2025-12-06 16:05:13 +0700

    update tc resgiter

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    // Sesuaikan dengan URL lokal project Laravel Anda saat dijalankan (php artisan serve)
+    baseUrl: 'http://127.0.0.1:8000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
